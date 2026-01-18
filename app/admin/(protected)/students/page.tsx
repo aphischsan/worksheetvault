6b9fa877cf7af74d7ab22591a970de0a5d3f141d
aphischsan <aphisch.san@gmail.com>
2026-01-19 00:43:22 +0800

    Update page.tsx

--- app/admin/(protected)/students/page.tsx
+++ app/admin/(protected)/students/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { upsertStudent, resetStudentPin } from "@/lib/actions";
 import { supabaseServer } from "@/lib/supabase";
 
